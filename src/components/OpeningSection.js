b9e95990a4488a6c8ae10ae090b1d41f74c73518
lyallbeveridge <lyall.beveridge@gmail.com>
2024-05-03 16:06:39 +1000

    Changed around the section gappage

--- src/components/OpeningSection.js
+++ src/components/OpeningSection.js
@@ -9,40 +9,18 @@ function OpeningSection() {
       {/* <GlobeAnimation/> */}
       <ProfileAnimation />
       {/* <HardwareAnimation /> */}
-      <canvas style={{width: "100px", height: "100px", backgroundColor: "whitesmoke"}}>
-
-      </canvas>
-      <br />
-      {
-        "G'day! My name is Lyall Beveridge, and this is my digital portfolio. Here, you'll find a showcase of my engineering and computer science projects, each detailed with insights into my creative process and the technologies I use."
-      }
-      <br />
-      <br />
-      {
-        "This website itself is not only a demonstration of my frontend web development skills but is also a testament to hackerzines, which were famous in the the early ???'s. [LINK]"
-      }
-      {/* <div>
-        <br />
-        {"> "}
-        <a
-          href="https://www.linkedin.com/in/lyall-beveridge"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <br />
-        {"> "}
-        <a
-          href="https://github.com/lyallbeveridge"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <br />
-        {"> "} <a href="mailto:lyall.beveridge@gmail.com">Email</a>
-      </div> */}
+      <canvas
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: "whitesmoke",
+        }}
+      ></canvas>
+      <pre>
+        {
+          "G'day! My name is Lyall Beveridge, and this is my digital portfolio. Here, you'll find a showcase of my engineering and computer science projects, each detailed with insights into my creative process and the technologies I use.\n\nThis website itself is not only a demonstration of my frontend web development skills but is also a testament to hackerzines, which were famous in the the early ???'s. [LINK]"
+        }
+      </pre>
     </>
   );
 }
