b79943869a17d652fc7a20224853f3f87d9da7d2
lyallbeveridge <lyall.beveridge@gmail.com>
2024-05-03 15:16:26 +1000

    Updates

--- src/components/SectionContent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SectionContent({ children, containerDivId }) {
-  return (
-    <div id={containerDivId} className="section-content">
-      {children}
-    </div>
-  );
-}
--- src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateString) {
-    const [day, month, year] = dateString.split('-');
-    const correctFormatDate = `${month}-${day}-${year}`;
-    const date = new Date(correctFormatDate);
-    const options = { month: 'short', year: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
-    return formattedDate;
-}
