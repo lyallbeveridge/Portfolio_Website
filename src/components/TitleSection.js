c90f8c632767d1bfde396b6c7be45871af6d7879
lyallbeveridge <lyall.beveridge@gmail.com>
2024-05-03 14:52:40 +1000

    Cleanup, about to try and gh pages publish

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -5,41 +5,40 @@ import "../styles/titleSection.css";
 import SubtitleAnimation from "./SubtitleAnimation";
 
 export default function TitleSection() {
-
-
   return (
     <>
       <TitleASCII />
       <SubtitleAnimation />
       <div id="contents-links">
-        <div className="content-link">
-          {"> "}&nbsp;
-          <a
-            href="#skills"
-            onClick={(event) => {
-              event.preventDefault();
-              document
-                .querySelector("#skills")
-                .scrollIntoView({ behavior: "smooth" });
-            }}
-          >
-            Skills
-          </a>
-        </div>
-        <div className="content-link">
-          {"> "}&nbsp;
-          <a
-            href="#projects"
-            onClick={(event) => {
-              event.preventDefault();
-              document
-                .querySelector("#projects")
-                .scrollIntoView({ behavior: "smooth" });
-            }}
-          >
-            Projects
-          </a>
-        </div>
+        <ul>
+          <li>
+            {" "}
+            <a
+              href="#skills"
+              onClick={(event) => {
+                event.preventDefault();
+                document
+                  .querySelector("#skills")
+                  .scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Skills
+            </a>
+          </li>
+          <li>
+            <a
+              href="#projects"
+              onClick={(event) => {
+                event.preventDefault();
+                document
+                  .querySelector("#projects")
+                  .scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Projects
+            </a>
+          </li>
+        </ul>
       </div>
       <IconLinkTray />
     </>
