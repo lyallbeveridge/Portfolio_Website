7fc352a7539d12eddf72a61be47c176e240c45db
lyallbeveridge <lyall.beveridge@gmail.com>
2024-06-07 14:28:47 +1000

    Finished a draft of the landing section

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -5,58 +5,63 @@ import IconLinkTray from "./IconLinkTray";
 import SubtitleAnimation from "./SubtitleAnimation";
 import ProfileAnimation from "./ProfileAnimation";
 
-
 export default function TitleSection() {
   return (
     <div id="title-section">
-      <ProfileAnimation />
-      <TitleASCII />
-      <SubtitleAnimation />
-      <div id="contents-links-container">
-        <ul id="contents-links-ul">
-          <li>
-            {" "}
-            <a
-              href="#about"
-              onClick={(event) => {
-                event.preventDefault();
-                document
-                  .querySelector("#about")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="#experience"
-              onClick={(event) => {
-                event.preventDefault();
-                document
-                  .querySelector("#experience")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Experience
-            </a>
-          </li>
-          <li>
-            <a
-              href="#projects"
-              onClick={(event) => {
-                event.preventDefault();
-                document
-                  .querySelector("#projects")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Projects
-            </a>
-          </li>
-        </ul>
+      <div className="title-third">
+        <ProfileAnimation />
       </div>
-      <IconLinkTray />
+      <div className="title-third" id="title-center-third">
+        <TitleASCII />
+        <SubtitleAnimation />
+        <div id="contents-links-container">
+          <ul id="contents-links-ul">
+            <li>
+              {" "}
+              <a
+                href="#about"
+                onClick={(event) => {
+                  event.preventDefault();
+                  document
+                    .querySelector("#about")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#experience"
+                onClick={(event) => {
+                  event.preventDefault();
+                  document
+                    .querySelector("#experience")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                Experience
+              </a>
+            </li>
+            <li>
+              <a
+                href="#projects"
+                onClick={(event) => {
+                  event.preventDefault();
+                  document
+                    .querySelector("#projects")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="title-third">
+        <IconLinkTray />
+      </div>
     </div>
   );
 }
